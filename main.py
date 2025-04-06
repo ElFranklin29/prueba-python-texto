6ec7dd7feee70d2ffe29fd00ddb01298bc84c88a
Franklin <franklin2908david@gmail.com>
2025-04-05 20:03:44 -0500

    Se sube el codigo con la logica completa. Se agrega logica para analizar texto de acuerdo a los requerimientos, ademas se agrega codigo necesario para manipular archivos .txt

---
+++ main.py
@@ -0,0 +1,79 @@
+from collections import Counter
+from pprint import PrettyPrinter
+import regex
+from obtenerArchivo import leer_archivo
+import sys
+
+def menu():
+    opcion = input("\n¡Bienvenido a TextoInspector3000 El inspector de palabras definitivo: cuenta, limpia y revela secretos del texto!\n"
+            "\n¿Como deseas ingresar el parrafo?\n"
+            "\n1. Ingresar parrafo desde consola"
+            "\n2. Ingresar parrafo desde un archivo .txt"
+            "\n3. Ingresar parrafo desde el codigo"
+                
+            "\n¡IMPORTANTE! Tenga en cuenta que para la opcion 2, el codigo debio ser ejecutado con un argumento, el cual debe ser el nombre del archivo .txt\n"
+               
+            "\nSelecciona una opcion: ")
+    match opcion:
+        case "1":
+            parrafo=input("Ingrese el texto que quiere analizar: ")
+        case "2":
+            parrafo = leer_archivo()
+        case "3":
+            parrafo = "La evolución de la tecnología ha sido, sin lugar a dudas, un fenómeno imparable. Sin embargo, ¿hemos considerado realmente sus efectos a largo plazo? Algunos expertos, como la Dra. González, advierten sobre los riesgos de una dependencia excesiva. Otros, en cambio, celebran los avances como una muestra del ingenio humano." 
+        case _:
+            print("ERROR Ingrese una opcion valida")
+            sys.exit(1)
+    procesar_texto(parrafo)    
+    
+    
+
+def procesar_texto(parrafo: str):
+    
+    #Se eliminan signos de puntuacion con libreria regex e indicando categoria unicode
+    textoLimpio = regex.sub(r"\p{P}","", parrafo.lower())
+
+    #Obtener cada palabra del texto
+    listaPalabras = textoLimpio.split()
+    
+    #Obtener numero de palabras mediante el tamaño de la lista
+    numeroPalabras = len(listaPalabras)
+    
+    #Obtener todas las palabras sin repetir
+    palabrasUnicas = set(listaPalabras)
+
+    #Obtener palabra mas larga
+    palabrasMasLargas = []
+    tamPalabra = len(max(palabrasUnicas, key=len))
+    for palabra in palabrasUnicas:
+        if tamPalabra == len(palabra):
+            palabrasMasLargas.append(palabra)
+    
+    #Obtener numero de veces que aparece cada palabra
+    cantidadPorPalabra = Counter(listaPalabras)
+    
+    resultado = {
+        "Parrafo": parrafo, 
+        "Numero de palabras": numeroPalabras,
+        "Lista de palabras (Sin repetir)": palabrasUnicas,
+        "Palabra(s) mas larga": palabrasMasLargas,
+        "Numero de veces que aparece cada palabra": cantidadPorPalabra
+        }
+    
+
+    pp = PrettyPrinter(sort_dicts=False)
+    print("\nTEXTO ANALIZADO\n")
+    pp.pprint(resultado)
+    
+    
+
+
+
+
+
+menu()
+
+
+
+
+
---
+++ obtenerArchivo.py
@@ -0,0 +1,18 @@
+import sys
+
+def leer_archivo():
+    #Comprobar que se envio como argumento solo el archivo .txt
+    if  len(sys.argv) < 2 or len(sys.argv) > 2:
+        print("ERROR Debe seguir el siguiente formato al ejecutar el codigo: python codigo_main.py nombre_archivo.txt")
+        sys.exit(1)
+ 
+
+    nombreArchivo = sys.argv[1]
+
+    try:
+        with open(nombreArchivo, "r", encoding="utf-8") as archivo:
+            contenido = archivo.read()
+    except FileNotFoundError:
+        print(f"ERROR el archivo no existe, digite nuevamente el nombre: {nombreArchivo}")
+
+    return contenido
